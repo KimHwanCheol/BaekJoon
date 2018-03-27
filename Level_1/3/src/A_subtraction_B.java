f4281f93c8b81de84e76107732da8794012c2186
KimHwanCheol <jkgbrj@naver.com>
2018-03-27 10:51:23 +0900

    Level 1_3 A-B

---
+++ Level_1/3/src/A_subtraction_B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class A_subtraction_B {
+	public static void main(String args[]) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a= sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a-b);
+	}
+}
