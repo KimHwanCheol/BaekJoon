1b32def1a8b5d89057c82ecadae7895399cdb3a8
KimHwanCheol <jkgbrj@naver.com>
2018-03-27 15:51:01 +0900

    Level 1_6 입력받은 내용 출력

---
+++ Level_1/6/src/Print.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Print {
+	public static void main(String args[]) {
+		
+		String a,b,c;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.next();
+		b = sc.next();
+		c = sc.next();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
