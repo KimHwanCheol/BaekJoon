4fc29befb65ffb4f8355a35c3496126ce43820b0
KimHwanCheol <jkgbrj@naver.com>
2018-03-27 14:04:35 +0900

    Level 1_3 Dog (modification 1)

--- Level_1/4/src/Dog.java
+++ Level_1/4/src/Dog.java
@@ -4,7 +4,7 @@ public class Dog {
         
     	System.out.println("|\\_/|");
 		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\"\\");
+		System.out.println("( 0 )\"\"\"\\");
 		System.out.println("|\"^\"`   |");
 		System.out.println("||_/=\\__|");
         
