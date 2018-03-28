6cc1fddbb808a508537ab6e5957aed8723cbb429
KimHwanCheol <jkgbrj@naver.com>
2018-03-28 11:02:31 +0900

    Level 1-8 입력받은대로 출력2 (modification 1)

--- Level_1/8/src/Print2.java
+++ Level_1/8/src/Print2.java
@@ -1,19 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Print2 {
 	public static void main(String args[]) {
 		
-		String a,b,c,d;
 		Scanner sc = new Scanner(System.in);
 		
-		a = sc.nextLine();
-		b = sc.nextLine();
-		c = sc.nextLine();
-		d = sc.nextLine();
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		sc.close();
 	}
 }
