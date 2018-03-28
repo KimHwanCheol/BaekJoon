f7356df3218acf49233590e8bb0054749be37f39
KimHwanCheol <jkgbrj@naver.com>
2018-03-28 15:34:51 +0900

    Level 2_7 A+B

---
+++ Level_2/7/src/A_add_B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A_add_B {
+	public static void main(String args[]) {
+	
+		int num1, num2;
+		
+		Scanner sc = new Scanner(System.in);
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		
+		System.out.println(num1+num2);
+	}
+}
