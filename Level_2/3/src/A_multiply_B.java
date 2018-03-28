3eeb70873ddec3124e6c889fa102e495d5be171c
KimHwanCheol <jkgbrj@naver.com>
2018-03-28 11:08:19 +0900

    Level 2_3 A*B

---
+++ Level_2/3/src/A_multiply_B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class A_multiply_B {
+	public static void main(String args[]) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();
+
+		System.out.println(a*b);
+	}
+}
