d286cf8fb5e28672fcb4d8de7ade73fc448c70f0
KimHwanCheol <jkgbrj@naver.com>
2018-03-28 10:59:19 +0900

    Level 1-7 입력받은대로 출력 (modification 1)

--- Level_1/7/src/Print.java
+++ Level_1/7/src/Print.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class Print {
 	public static void main(String args[]) {
 		
-		String a,b,c;
 		Scanner sc = new Scanner(System.in);
 		
-		a = sc.nextLine();
-		b = sc.nextLine();
-		c = sc.nextLine();
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+		sc.close();
 	}
 }
--- Level_1/8/src/Print2.java
+++ Level_1/8/src/Print2.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Print2 {
-
+	public static void main(String args[]) {
+		
+		String a,b,c,d;
+		Scanner sc = new Scanner(System.in);
+		
+		a = sc.nextLine();
+		b = sc.nextLine();
+		c = sc.nextLine();
+		d = sc.nextLine();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
 }
