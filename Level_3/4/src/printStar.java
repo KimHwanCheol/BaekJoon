0f9844e519cb4623c55cef1cdaa90c0f994ecb90
KimHwanCheol <jkgbrj@naver.com>
2018-03-30 14:25:46 +0900

    Level 3_4 별 찍기

---
+++ Level_3/4/src/printStar.java
@@ -0,0 +1,18 @@
+import java.util.*;
+//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+public class printStar {
+	public static void main(String args[]) {
+		
+		int inputNum;
+		
+		Scanner sc = new Scanner(System.in);
+		inputNum = sc.nextInt();
+		
+		for(int i = 1; i<=inputNum;i++) {
+			for(int j = 1; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
