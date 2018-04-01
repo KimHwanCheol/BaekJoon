4101b481e0935816b5581025f9d057546feeaae9
KimHwanCheol <jkgbrj@naver.com>
2018-04-01 15:11:05 +0900

    Level 3_10 숫자의 합 (modification 1)

--- Level_3/10/src/totalAdd.java
+++ Level_3/10/src/totalAdd.java
@@ -1,22 +1,26 @@
 import java.util.*;
+import java.lang.*;
+
+//N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
 
 public class totalAdd {
 	public static void main(String args[]) {
 		
 		int num1; //두번째 입력으로 주어질 숫자의 갯수
-		long num2;
-		long result = 0;
+		String num2 = "";
+		int result = 0;
 				
 		Scanner sc = new Scanner(System.in);
-		num1 = sc.nextInt();
-		num2 = sc.nextLong();
+		num1 = sc.nextInt(); //num1을 int형으로 받아서 배열의 크기를 정하고
+		num2 = sc.next(); //num2를 String 형으로 받아서 totalNum배열에 저장
 		
-		for(long i=0,j=(long) Math.pow(10, num1-1);i<num1;i++,j/=10) {
-			result = result + num2/j;
-			num2 = num2 - ((num2/j)*j);
-		}
-		
+		char[] totalNum = new char[num1]; 
+		totalNum = num2.replaceAll("", "").toCharArray();
+
+		for(int i =0;i<num1;i++) {
+			result = result + Integer.parseInt(String.valueOf(totalNum[i]));
+		}//배열 내부 각각의 인덱스를 숫자로 변환하여 result에 더함
+
 		System.out.println(result);
-		
 	}
 }
